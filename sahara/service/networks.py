17ef93940ee3b7c35bea349a2d6d9bdb701c85a8
henaras <henaras@gmail.com>
2015-05-06 12:17:42 -0400

    modified juno version to work with SAVI.

--- sahara/service/networks.py
+++ sahara/service/networks.py
@@ -4,7 +4,7 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#    http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -41,47 +41,59 @@ def init_instances_ips(instance):
     management_ip = None
     internal_ip = None
 
+    # for network_label, addresses in six.iteritems(server.addresses):
+    #     for address in addresses:
+    #         if address['OS-EXT-IPS:type'] == 'fixed':
+    #             internal_ip = internal_ip or address['addr']
+    #         else:
+    #             management_ip = management_ip or address['addr']
+
     for network_label, addresses in six.iteritems(server.addresses):
         for address in addresses:
-            if address['OS-EXT-IPS:type'] == 'fixed':
-                internal_ip = internal_ip or address['addr']
+            if 'OS-EXT-IPS:type' in address:
+                if address['OS-EXT-IPS:type'] == 'fixed':
+                    internal_ip = internal_ip or address['addr']
+                else:
+                    management_ip = management_ip or address['addr']
             else:
+                internal_ip = internal_ip or address['addr']
                 management_ip = management_ip or address['addr']
 
-    if not CONF.use_floating_ips:
-        management_ip = internal_ip
 
-    # NOTE(aignatov): Once bug #1262529 is fixed this 'if' block should be
-    # reviewed and reformatted again, probably removed completely.
-    if CONF.use_neutron and not (management_ip and internal_ip):
-        LOG.debug("Instance %s doesn't contain yet Floating IP or Internal IP."
-                  " Floating IP=%s, Internal IP=%s. Trying to get via Neutron."
-                  % (server.name, management_ip, internal_ip))
-        neutron_client = neutron.client()
-        ports = neutron_client.list_ports(device_id=server.id)["ports"]
-        if ports:
-            target_port_id = ports[0]['id']
-            fl_ips = neutron_client.list_floatingips(
-                port_id=target_port_id)['floatingips']
-            if fl_ips:
-                fl_ip = fl_ips[0]
-                if not internal_ip:
-                    internal_ip = fl_ip['fixed_ip_address']
-                    LOG.debug('Found fixed IP %s for %s' % (internal_ip,
-                                                            server.name))
-                # Zeroing management_ip if Sahara in private network
-                if not CONF.use_floating_ips:
-                    management_ip = internal_ip
-                elif not management_ip:
-                    management_ip = fl_ip['floating_ip_address']
-                    LOG.debug('Found floating IP %s for %s' % (management_ip,
-                                                               server.name))
+if not CONF.use_floating_ips:
+    management_ip = internal_ip
 
-    conductor.instance_update(context.ctx(), instance,
-                              {"management_ip": management_ip,
-                               "internal_ip": internal_ip})
+# NOTE(aignatov): Once bug #1262529 is fixed this 'if' block should be
+# reviewed and reformatted again, probably removed completely.
+if CONF.use_neutron and not (management_ip and internal_ip):
+    LOG.debug("Instance %s doesn't contain yet Floating IP or Internal IP."
+              " Floating IP=%s, Internal IP=%s. Trying to get via Neutron."
+              % (server.name, management_ip, internal_ip))
+    neutron_client = neutron.client()
+    ports = neutron_client.list_ports(device_id=server.id)["ports"]
+    if ports:
+        target_port_id = ports[0]['id']
+        fl_ips = neutron_client.list_floatingips(
+            port_id=target_port_id)['floatingips']
+        if fl_ips:
+            fl_ip = fl_ips[0]
+            if not internal_ip:
+                internal_ip = fl_ip['fixed_ip_address']
+                LOG.debug('Found fixed IP %s for %s' % (internal_ip,
+                                                        server.name))
+            # Zeroing management_ip if Sahara in private network
+            if not CONF.use_floating_ips:
+                management_ip = internal_ip
+            elif not management_ip:
+                management_ip = fl_ip['floating_ip_address']
+                LOG.debug('Found floating IP %s for %s' % (management_ip,
+                                                           server.name))
 
-    return internal_ip and management_ip
+conductor.instance_update(context.ctx(), instance,
+    {"management_ip": management_ip,
+     "internal_ip": internal_ip})
+
+return internal_ip and management_ip
 
 
 def assign_floating_ip(instance_id, pool):
@@ -93,3 +105,7 @@ def delete_floating_ip(instance_id):
     fl_ips = nova.client().floating_ips.findall(instance_id=instance_id)
     for fl_ip in fl_ips:
         nova.client().floating_ips.delete(fl_ip.id)
+
+
+def list_networks(**kwargs):
+    return neutron.client().list_networks(**kwargs)['networks']
