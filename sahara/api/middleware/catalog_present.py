875d2f10351dc4a709b0c787acca5ebe1df5950f
henaras <henaras@gmail.com>
2015-07-14 17:55:56 -0400

    Added for SAVI.

--- sahara/api/middleware/catalog_present.py
+++ sahara/api/middleware/catalog_present.py
@@ -14,7 +14,7 @@
 # limitations under the License.
 
 from keystoneclient import access
-from sahara.openstack.common import jsonutils
+from oslo_serialization import jsonutils
 from keystoneclient.v2_0 import client as keystone_client
 
 from sahara.openstack.common import log as logging
